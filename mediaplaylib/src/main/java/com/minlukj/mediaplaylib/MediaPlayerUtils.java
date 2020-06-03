006ee9abb3ba816bbaf73f5b52758c2b10beb4c2
gaoguofeng <a912816369@gmail.com>
2020-06-03 10:54:28 +0800

    修改：注释、初始化传入Context调用方法不需要再传入Context，初始化未传入Context调用方法需要传递Context。

--- mediaplaylib/src/main/java/com/minlukj/mediaplaylib/MediaPlayerUtils.java
+++ mediaplaylib/src/main/java/com/minlukj/mediaplaylib/MediaPlayerUtils.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.os.Message;
 import java.io.File;
 import java.io.IOException;
 
@@ -16,8 +17,10 @@ public class MediaPlayerUtils {
   public static final int PLAY_STATE1 = 2;//raw
   public static final int PLAY_STATE2 = 3;//assets
   public static final int PLAY_STATE3 = 4;//网络
+  public static final int IS_PLAY_STOP = 100;//是否停止播放
   private int duration;
   private boolean isPlaying = false;
+
 
   //播放文件的路径
   private File targetFile;
@@ -37,7 +40,18 @@ public class MediaPlayerUtils {
   //多久获取一次进度 默认500毫秒
   private int sleep = 500;
 
-  private Handler mHandler = new Handler();
+  private Handler mHandler = new Handler(new Handler.Callback() {
+    @Override public boolean handleMessage(Message msg) {
+      //停止前发送进度.
+      if (mMediaPlayInfoListener != null && mMediaPlayer != null && isPlaying())
+        mMediaPlayInfoListener.onSeekBarProgress(mMediaPlayer.getCurrentPosition());
+
+      if (msg.what == IS_PLAY_STOP && mHandler != null){
+        mHandler.removeCallbacks(mRunnable);
+      }
+      return false;
+    }
+  });
   private Runnable mRunnable = new Runnable() {
     @Override public void run() {
       if (mMediaPlayInfoListener != null && mMediaPlayer != null && isPlaying())
@@ -46,34 +60,88 @@ public class MediaPlayerUtils {
     }
   };
 
-  //设置文件路径
+  public MediaPlayerUtils() {
+  }
+
+  public MediaPlayerUtils(Context context) {
+    mContext = context;
+  }
+
+
+  /**
+   * 设置文件路径
+   * @param file 需要播放文件的路径
+   */
   public void setFilePlay(File file) {
     this.targetFile = file;
     PLAY_STATE = PLAY_STATE0;
     stop();
   }
-  //设置Raw播放
+
+  /**
+   * 设置Raw播放
+   * @param rawId R.raw.music3
+   */
   public void setRawPlay(Context context,int rawId){
     this.mContext = context;
     this.rawId = rawId;
     PLAY_STATE = PLAY_STATE1;
     stop();
   }
-  //设置Assets播放
+
+  /**
+   * 设置Raw播放
+   * 调用此方法必须在初始化时传入Context
+   * @param rawId R.raw.music3
+   */
+  public void setRawPlay(int rawId){
+    if (mContext == null){
+      throw new NullPointerException("Context Null");
+    }
+    this.rawId = rawId;
+    PLAY_STATE = PLAY_STATE1;
+    stop();
+  }
+
+  /**
+   * 设置Assets播放
+   * @param assetsName assets文件名
+   */
   public void setAssetsName(Context context,String assetsName) {
     this.mContext = context;
     this.assetsName = assetsName;
     PLAY_STATE = PLAY_STATE2;
     stop();
   }
-  //设置网络资源播放
+
+  /**
+   * 设置Assets播放
+   * 调用此方法必须在初始化时传入Context
+   * @param assetsName assets文件名
+   */
+  public void setAssetsName(String assetsName) {
+    if (mContext == null){
+      throw new NullPointerException("Context Null");
+    }
+    this.assetsName = assetsName;
+    PLAY_STATE = PLAY_STATE2;
+    stop();
+  }
+
+  /**
+   * 设置网络资源播放
+   * @param netPath 网络音乐地址
+   */
   public void setNetPath(String netPath) {
     this.netPath = netPath;
     PLAY_STATE = PLAY_STATE3;
     stop();
   }
 
-  //开始播放
+  /**
+   * 开始播放
+   * @return true 开始播放， false 播放错误
+   */
   public boolean start() {
     if (PLAY_STATE == PLAY_STATE1){
       mMediaPlayer = MediaPlayer.create(mContext,rawId);
@@ -104,7 +172,6 @@ public class MediaPlayerUtils {
           if (mMediaPlayInfoListener != null)
             mMediaPlayInfoListener.onCompletion(mediaPlayer);
           stop();
-
         }
       });
       //准备完毕 自动播放
@@ -147,11 +214,7 @@ public class MediaPlayerUtils {
       });
     } catch (IOException e) {
       e.printStackTrace();
-
-      mMediaPlayer.stop();
-      mMediaPlayer.reset();
-      mMediaPlayer.release();
-      mMediaPlayer = null;
+      stop();
     }
 
     boolean result = (mMediaPlayer != null);
@@ -160,13 +223,17 @@ public class MediaPlayerUtils {
     return result;
   }
 
-  //停止播放
+  /**
+   *   停止播放
+   */
   public void stop() {
     this.isPlaying = false;
     this.duration = 0;
 
     if (mMediaPlayer != null) {
-      mHandler.removeCallbacks(mRunnable);
+      Message message = new Message();
+      message.what = IS_PLAY_STOP;
+      mHandler.sendMessage(message);
       mMediaPlayer.stop();
       mMediaPlayer.reset();
       if (mediaPlayFunctionListener != null)
@@ -176,7 +243,9 @@ public class MediaPlayerUtils {
     }
   }
 
-  //resume调用 继续播放也调用此方法即可
+  /**
+   * resume调用 继续播放也调用此方法即可
+   */
   public void resume() {
     if (mMediaPlayer != null) {
       this.isPlaying = true;
@@ -185,7 +254,9 @@ public class MediaPlayerUtils {
     }
   }
 
-  //暂停
+  /**
+   * 暂停
+   */
   public void pause() {
     this.isPlaying = false;
     mHandler.removeCallbacks(mRunnable);
@@ -196,18 +267,26 @@ public class MediaPlayerUtils {
     }
   }
 
-  //是否正在运行
+  /**
+   * 是否正在运行
+   * @return true 正在运行，false停止运行
+   */
   public boolean isRunning() {
     return (mMediaPlayer != null);
   }
 
-  //是否在播放中
+  /**
+   * 是否在播放中
+   * @return true 正在播放，false 停止播放
+   */
   public boolean isPlaying() {
     return this.isPlaying;
   }
 
-
-  //播放文件的时长
+  /**
+   * 播放文件的时长
+   * @return 文件时长
+   */
   public int getDuration() {
     if (mMediaPlayer == null) {
       return this.duration;
@@ -216,7 +295,10 @@ public class MediaPlayerUtils {
     return mMediaPlayer.getDuration();
   }
 
-  //获取当前播放位置
+  /**
+   * 获取当前播放位置
+   * @return 当前播放位置值
+   */
   public int getCurrentPosition() {
     if (mMediaPlayer == null) {
       return 0;
@@ -225,61 +307,95 @@ public class MediaPlayerUtils {
     return mMediaPlayer.getCurrentPosition();
   }
 
-  //左右声道大小 0-1
+  /**
+   * 左右声道大小
+   * @param leftVolume 左声道大小 0 - 1
+   * @param rightVolume 右声道大小 0 - 1
+   */
   public void setVolume(float leftVolume, float rightVolume){
     if (mMediaPlayer != null){
       mMediaPlayer.setVolume(leftVolume,rightVolume);
     }
   }
 
-  //设置唤醒方式 需要在清单文件AndroidManifest.xml中添加权限 <uses-permission android:name="android.permission.WAKE_LOCK" />
+  /**
+   * 设置唤醒方式 需要在清单文件AndroidManifest.xml中添加权限 <uses-permission android:name="android.permission.WAKE_LOCK" />
+   * @param context 上下文
+   * @param mode 唤醒模式
+   */
   public void setWakeMode(Context context, int mode){
     if (mMediaPlayer != null)
       mMediaPlayer.setWakeMode(context,mode);
   }
 
-  //播放时不熄屏
+  /**
+   * 播放时不熄屏
+   * @param screenOn true 不息屏，false 息屏
+   */
   public void setScreenOnWhilePlaying(boolean screenOn){
     if (mMediaPlayer != null)
       mMediaPlayer.setScreenOnWhilePlaying(screenOn);
   }
 
-  //指定播放位置 毫秒
+  /**
+   * 指定播放位置 毫秒
+   * @param msec 要播放的值
+   */
   public void seekTo(int msec){
     if (mMediaPlayer != null)
       mMediaPlayer.seekTo(msec);
   }
 
-  //是否循环播放
+  /**
+   * 是否循环播放
+   * @param looping true 循环播放，false 不循环
+   */
   public void setLooping(boolean looping){
     if (mMediaPlayer != null)
       mMediaPlayer.setLooping(looping);
   }
 
-  //获取当前播放资源类型
+  /**
+   * 获取当前播放资源类型
+   * @return 当前资源类型
+   */
   public int getMusicType(){
     return PLAY_STATE;
   }
 
-  //必须调用此方法
+  /**
+   * 必须调用此方法 销毁，释放
+   */
   public void destory(){
     stop();
   }
 
-  //多久获取一次进度 毫秒
+  /**
+   * 多久获取一次进度 毫秒
+   * @param sleep 默认500
+   */
   public void setSleep(int sleep){
     this.sleep = sleep;
   }
 
+  /**
+   * 获取MediaPlayer对象
+   * @return MediaPlayer
+   */
   public MediaPlayer getMediaPlayer() {
     return this.mMediaPlayer;
   }
 
-
+  /**
+   * 功能监听
+   */
   public void setMediaPlayFunctionListener(MediaPlayFunctionListener mediaPlayFunctionListener){
     this.mediaPlayFunctionListener = mediaPlayFunctionListener;
   }
 
+  /**
+   * 播放信息监听
+   */
   public void setMediaPlayInfoListener(MediaPlayInfoListener mediaPlayInfoListener) {
     this.mMediaPlayInfoListener = mediaPlayInfoListener;
   }
